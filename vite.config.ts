2ccc9e1e6004f0daa03acef682e94e3fb7497cc8
VLineDevelopment <vlinedevelopment@gmail.com>
2025-01-18 04:19:03 -0800

    added route

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vlinelanding/', // Add this line - should match your repository name
+
   plugins: [react()],
   resolve: {
     alias: {
